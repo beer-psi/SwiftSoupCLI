8d401056c3142141c9b17b9c60a4f0aaa52040f5
beerpiss <lacvtg.a1.2023@gmail.com>
2022-07-09 12:06:58 +0700

    add option to specify url, add swiftlint

--- Sources/SwiftSoupCLI/main.swift
+++ Sources/SwiftSoupCLI/main.swift
@@ -1,4 +1,5 @@
 import Foundation
+import FoundationNetworking
 import ArgumentParser
 import SwiftSoup
 
@@ -23,8 +24,11 @@ struct SwiftSoupCLI: ParsableCommand {
 	@Argument(help: "The CSS selector to use.")
 	private var selector: String
 
-	@Argument(help: "The HTML to parse. If not provided, or the HTML is \"-\", the HTML will be read from stdin.")
-	private var html: String = "-"
+	@Argument(help: "The HTML to parse. If -, read from stdin.")
+	private var html: String?
+
+	@Option(name: .shortAndLong, help: "The URL to fetch HTML content from.")
+	private var url: String?
 
 	@Option(name: .shortAndLong, help: "Extract attribute from selected elements")
 	private var attribute: String?
@@ -54,23 +58,27 @@ struct SwiftSoupCLI: ParsableCommand {
 	private var outputType: OutputTypes = .outerHtml
 
 	mutating func run() throws {
-		if html == "-" {
-			let file = FileHandle.standardInput
-			if let data = try? file.readToEnd() {
-				if let html = String(data: data, encoding: .utf8) {
-					self.html = html
-				} else {
-					print("Error: No input given.")
-					_exit(1)
-				}
-			} else {
-				print("Error: Could not read from stdin.")
-				_exit(1)
-			}
+		let _html: String;
+		if html == nil,
+			let urlstr = url,
+			let url = URL(string: urlstr),
+			let str = try? String(contentsOf: url, encoding: .utf8)
+		{	
+			_html = str
+		} else if html == nil || html == "-", 
+			let data = try? FileHandle.standardInput.readToEnd(),
+			let str = String(data: data, encoding: .utf8)
+		{
+			_html = str
+		} else if let html = html {
+			_html = html
+		} else {
+			print("Error: No input given.")
+			return
 		}
 
 		do {
-			let elements = try SwiftSoup.parse(html).select(selector)
+			let elements = try SwiftSoup.parse(_html, url ?? "").select(selector)
 			for element in elements.array() {
 				var output: String?;
 				if let attribute = attribute {
